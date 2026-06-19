94ba89d811a1466e79d45279792478c66105a854
Matt Shay <mattshay18@icloud.com>
2026-06-19 12:15:21 -0500

    Setting up the dashboardDemo, fake data half way done. fakePendingAppointments, businessName, fakeCompletedAppointments. add fakeStatCards and fakeAlerts

---
+++ src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,15 @@
+import type { DashboardProps } from "./dashboard.types";
+
+function Dashboard({ todaysAppointments, completedAppointments, statCards, alerts }: DashboardProps) {
+    
+    return (
+    console.log(new Date()),
+    <div>
+      Dashboard placeholder
+    </div>
+  
+  );
+}
+
+export default Dashboard;
+
